1ded9c7f11551d61ece514c0717fd342a89b9135
Luis Silveira <104040064+LuisSilveiraALX@users.noreply.github.com>
2023-09-06 11:45:09 -0300

    Upload responsive icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,20 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta
+            name="description"
+            content="Web site created using ReactJS, NextJS and TypeScript"
+            />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;700&display=swap" rel="stylesheet" />
+          <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta property="og:locale" content="en_US" />
+          <title>Home | Luis Silveira</title>
+          
         </Head>
         <body>
           <Main />
